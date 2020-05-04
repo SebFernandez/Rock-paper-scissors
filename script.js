ea57753d47ba9515bb0ad152d4739987bf9a5a68
Seba Fernandez <seba.fernandez95@gmail.com>
2020-05-03 22:03:37 -0300

    Website working.

---
+++ script.js
@@ -0,0 +1,14 @@
+//Element ID's.
+let playerScore = document.getElementById ("player-score");
+let cpuScore = document.getElementById ("cpu-score");
+let playerChoice = document.getElementById ("decision-player");
+let cpuChoice = document.getElementById ("decision-cpu");
+
+//Images path.
+var scissorsIcon = "Images/hand-scissors-regular.svg"
+var rockIcon = "Images/hand-rock-regular.svg"
+var paperIcon = "Images/hand-paper-regular.svg"
+
+//Score counter.
+playerScore.textContent = 0;
+cpuScore.textContent = 0;
