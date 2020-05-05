59573b74737a2ab421ced0180997ccfa28733da8
Seba Fernandez <seba.fernandez95@gmail.com>
2020-05-04 21:02:00 -0300

    Made some progress.

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 //Element ID's.
-let playerScore = document.getElementById ("player-score");
-let cpuScore = document.getElementById ("cpu-score");
-let playerChoice = document.getElementById ("decision-player");
-let cpuChoice = document.getElementById ("decision-cpu");
+var playerScore = document.getElementById ("player-score");
+var cpuScore = document.getElementById ("cpu-score");
+var playerChoice = document.getElementById ("decision-player");
+var cpuChoice = document.getElementById ("decision-cpu");
+var playerRock = document.getElementById ("rock-icon");
+var playerPaper = document.getElementById("paper-icon");
+var playerScissors = document.getElementById("scissors-icon");
 
 //Images path.
-var scissorsIcon = "Images/hand-scissors-regular.svg"
-var rockIcon = "Images/hand-rock-regular.svg"
-var paperIcon = "Images/hand-paper-regular.svg"
+var scissorsIcon = "Images/hand-scissors-regular.svg";
+var rockIcon = "Images/hand-rock-regular.svg";
+var paperIcon = "Images/hand-paper-regular.svg";
+
+//Weapons
+var playerWeapon;
+var cpuWeapon;
 
 //Score counter.
-playerScore.textContent = 0;
+playerScore.textContent = 0;    
 cpuScore.textContent = 0;
+
+function randomWeapon ()    {
+    var choice = Math.round(Math.random () * 100) % 3;
+
+    if (choice == 0)    {
+        return 'scissors';
+    }   else if (choice == 1)   {
+        return 'rock';
+    }   else if (choice == 2)   {
+        return 'paper';
+    }
+}
+
+cpuWeapon = randomWeapon ();
